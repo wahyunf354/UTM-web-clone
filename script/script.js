bcd683bcc4d5fec84050d0f283d20a24cdb8e552
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-08-22 10:17:02 +0700

    finist

--- script/script.js
+++ script/script.js
@@ -1,25 +1,39 @@
 const nama = document.querySelector('input[name="nama"]');
 const nim = document.querySelector('input[name="nim"]');
 const gender = document.querySelectorAll('input[name="gender"]');
-const prodi = document.querySelector("select").value;
+const prodi = document.querySelector("select#prodi");
 const button = document.querySelector(".btn");
-const password = document.querySelector('input[name="password"]').value;
+const password = document.querySelector('input[name="password"]');
+const konfirmasiPassword = document.querySelector("#konfirmasiPassword");
+const buttonRegister = document.querySelector("div.hero div.container a");
+
 const validateName = document.querySelector("#validate-nama p");
 const validateNim = document.querySelector("#validate-nim p");
-const konfirmasiPassword = document.querySelector(
-  'input[name="konfirmasiPassword"]'
-).value;
+const validateGender = document.querySelector("#validate-gender p");
+const validateProdi = document.querySelector("#validate-prodi p");
+const validatePassword = document.querySelector("#validate-conf-password p");
+
+const fieldset = document.querySelector("fieldset");
+buttonRegister.addEventListener("click", () => {
+  window.scrollTo({
+    top: 500,
+    behavior: "smooth",
+  });
+});
 
 button.addEventListener("click", function () {
   // Cek input nama
   var validasiAngka = /^[0-9]+$/;
+
   var validasiAngkaDua = /\D+/g;
+
   if (nama.value === "") {
     validateName.textContent = "Inputan Harus diisi";
-  } else if (nama.value.match(validasiAngkaDua)) {
+    return;
+  }
+  if (nama.value.match(validasiAngka)) {
     validateName.textContent = "Jangan memasukkan angka diisian nama";
-  } else {
-    validateName.textContent = "";
+    return;
   }
 
   // Cek NIM
@@ -27,24 +41,34 @@ button.addEventListener("click", function () {
   const cekAngka = /[0-9]$/g;
   if (nim.value === "") {
     validateNim.textContent = "masih kosong";
-  } else if (nim.value.match(cekAngka) && nim.value.length != 12) {
-    validateNim.textContent = "angka harus berjumlah 12";
-  } else {
-    validateNim.textContent = "Tidak valid";
+    return;
   }
-
+  if (nim.value.match(cekAngka) && nim.value.length != 12) {
+    validateNim.textContent = "angka harus berjumlah 12";
+    return;
+  }
   // cek isian gender
   if (!gender[0].checked & !gender[1].checked) {
-    alert("Pilih salah satu gender");
+    validateGender.textContent = "Pilih Woi";
+    return;
   }
 
   // Cek pilihan Prodi
-  if (prodi == "") {
-    alert("Pilih salah satu prodi");
+  if (prodi === "") {
+    validateProdi.textContent = "pilih Woi";
+    return;
   }
 
   // Cek isian konfirmasi password (konfirmasi password sudah sesuai atau belum)
-  if (password != konfirmasiPassword) {
-    alert("Konfirmasi password tidak sesuai dengan password asli");
+  if (password.value !== konfirmasiPassword.value) {
+    validatePassword.textContent = "passwordnya harus sama woi";
+    return;
   }
+
+  validateNim.textContent = "";
+  validateGender.textContent = "";
+  validateProdi.textContent = "";
+  validatePassword.textContent = "";
+  validateName.textContent = "";
+  fieldset.innerHTML = "<h2>Berhasil Registerasi</h2>";
 });
